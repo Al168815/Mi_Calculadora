7a7c3d4b33de71e5cf159a34936fec1c19251e8a
alumno <alumno@DESKTOP-H8Q40FO.uacj.mx>
2024-09-02 09:45:12 -0600

    ayudaaaaa

---
+++ Calculadora_/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calculadora_
+//
+//  Created by alumno on 9/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func que_hace_el_boton(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var botton_funcional: UIButton!
+}
+
